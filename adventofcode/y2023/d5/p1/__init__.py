f6cf249da24f0d4e4f8b9c8166fd8601d78ecb34
Adrian Miguel <adriantmiguel@gmail.com>
2023-12-05 19:24:15 -0800

    finished correct answer

--- adventofcode/y2023/d5/p1/__init__.py
+++ adventofcode/y2023/d5/p1/__init__.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import parsy
 from typing import Sequence
 
@@ -5,25 +6,24 @@ from adventofcode.helpers import executor
 
 
 NEWLINE = parsy.string('\n')
-NEWLINES = parsy.regex(r'\n+')
-MAYBE_NEWLINES = parsy.regex(r'\n*')
+NEWLINES = NEWLINE.at_least(1)
 
 SPACE = parsy.string(' ')
-SPACES = parsy.regex(r' +')
+SPACES = SPACE.at_least(1)
 
 NUMBER = parsy.decimal_digit.at_least(1).concat().map(int)
 NUMBER_LIST = NUMBER.sep_by(SPACES)
 
 SEEDS_LINE = parsy.string('seeds: ').then(NUMBER_LIST)
-MAPS_LINE = NUMBER.sep_by(SPACES, min=3, max=3)
+CATEGORY_MAPS_LINE = NUMBER.sep_by(SPACES, min=3, max=3)
 
 
 def create_maps_parser(name: str) -> parsy.Parser:
     return (
         parsy.string(f'{name} map:')
         .then(NEWLINE)
-        .then(MAPS_LINE.sep_by(NEWLINE, min=1))
-        .skip(MAYBE_NEWLINES)
+        .then(CATEGORY_MAPS_LINE.sep_by(NEWLINE, min=1))
+        .skip(NEWLINE.many())
     )
 
 
@@ -39,7 +39,43 @@ FILE = parsy.seq(
 )
 
 
-def parse_lines(*, lines: Sequence[str]) -> None:
+@dataclass(frozen=True, kw_only=True)
+class CategoryMap:
+    destination_start: int
+    length: int
+    source_start: int
+
+
+@dataclass(frozen=True, kw_only=True)
+class Almanac:
+    seeds: Sequence[int]
+    seed_to_soil: Sequence[CategoryMap]
+    soil_to_fertilizer: Sequence[CategoryMap]
+    fertilizer_to_water: Sequence[CategoryMap]
+    water_to_light: Sequence[CategoryMap]
+    light_to_temperature: Sequence[CategoryMap]
+    temperature_to_humidity: Sequence[CategoryMap]
+    humidity_to_location: Sequence[CategoryMap]
+
+
+def parse_category_map(*, map: Sequence[int]) -> CategoryMap:
+    destination_start, source_start, length = map
+
+    return CategoryMap(
+        destination_start=destination_start,
+        length=length,
+        source_start=source_start,
+    )
+
+
+def parse_category_maps(*, maps: Sequence[Sequence[int]]) -> Sequence[CategoryMap]:
+    return tuple(
+        parse_category_map(map=map)
+        for map in maps
+    )
+
+
+def parse_almanac(*, lines: Sequence[str]) -> Almanac:
     # TODO(adrian@gradient.ai, 12/05/2023): consider getting full file instead of lines
     file_content = '\n'.join(lines)
 
@@ -53,21 +89,49 @@ def parse_lines(*, lines: Sequence[str]) -> None:
         temperature_to_humidity_maps,
         humidity_to_location_maps,
     ) = FILE.parse(file_content)
-    print(seeds)
-    print(seed_to_soil_maps)
-    pass
+
+    return Almanac(
+        seeds=seeds,
+        seed_to_soil=parse_category_maps(maps=seed_to_soil_maps),
+        soil_to_fertilizer=parse_category_maps(maps=soil_to_fertilizer_maps),
+        fertilizer_to_water=parse_category_maps(maps=fertizlier_to_water_maps),
+        water_to_light=parse_category_maps(maps=water_to_light_maps),
+        light_to_temperature=parse_category_maps(maps=light_to_temperature_maps),
+        temperature_to_humidity=parse_category_maps(maps=temperature_to_humidity_maps),
+        humidity_to_location=parse_category_maps(maps=humidity_to_location_maps),
+    )
+
+
+def get_destination_value(*, category_maps: Sequence[CategoryMap], source: int) -> int:
+    for category_map in category_maps:
+        distance = source - category_map.source_start
+        if distance >= 0 and distance < category_map.length:
+            return category_map.destination_start + distance
+
+    return source
+
+
+def calculate_seed_location(*, almanac: Almanac, seed: int) -> int:
+    soil = get_destination_value(category_maps=almanac.seed_to_soil, source=seed)
+    fertilizer = get_destination_value(category_maps=almanac.soil_to_fertilizer, source=soil)
+    water = get_destination_value(category_maps=almanac.fertilizer_to_water, source=fertilizer)
+    light = get_destination_value(category_maps=almanac.water_to_light, source=water)
+    temperature = get_destination_value(category_maps=almanac.light_to_temperature, source=light)
+    humidity = get_destination_value(category_maps=almanac.temperature_to_humidity, source=temperature)
+    location = get_destination_value(category_maps=almanac.humidity_to_location, source=humidity)
+
+    return location
 
 
 def solution(lines: Sequence[str], /) -> int:
-    parse_lines(lines=lines)
-    # return sum(
-    #     calculate_points(
-    #         scratch_card=parse_scratch_card(line=line)
-    #     )
-    #     for line in lines
-    # )
+    almanac = parse_almanac(lines=lines)
+
+    return min(
+        calculate_seed_location(almanac=almanac, seed=seed)
+        for seed in almanac.seeds
+    )
 
 
 def main():
     executor.execute_example(solution)
-    # executor.execute_actual(solution)
+    executor.execute_actual(solution)
