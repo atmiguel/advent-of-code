5f24599fe779e620d8e9d2caeeb2466c822e124a
Adrian Miguel <adriantmiguel@gmail.com>
2023-12-06 20:35:47 -0800

    solved example

--- adventofcode/y2023/d6/p1/__init__.py
+++ adventofcode/y2023/d6/p1/__init__.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import functools
 import parsy
 from typing import Sequence
 
@@ -38,9 +39,30 @@ def parse_races(*, content: str) -> Sequence[Race]:
     )
 
 
+def calculate_win_count(*, race: Race) -> int:
+    half_time = race.time // 2
+    for index in range(half_time):
+        speed = index + 1
+        remaining_time = race.time - speed
+
+        if remaining_time * speed > race.distance:
+            result = (half_time - index) * 2
+            if race.time % 2 == 0:
+                result -= 1
+
+            return result
+
+    raise Exception('expected to find result')
+
+
 def solution(content: str, /) -> int:
     races = parse_races(content=content)
-    print(races)
+    win_counts = tuple(
+        calculate_win_count(race=race)
+        for race in races
+    )
+
+    return functools.reduce(lambda x, y: x * y, win_counts)
 
 
 def main():
