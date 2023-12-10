46da5580c7cc4e715f92324eacdf74b2ab024d45
Adrian Miguel <adriantmiguel@gmail.com>
2023-12-10 11:14:03 -0800

    did part 1

--- adventofcode/y2023/d9/p1/__init__.py
+++ adventofcode/y2023/d9/p1/__init__.py
@@ -1,106 +1,43 @@
-from dataclasses import dataclass
-import parsy
-from typing import Sequence, Dict
+from typing import Sequence
 
 from adventofcode.helpers import executor, parsers
 
 
-DIRECTION = parsy.regex(r'[RL]')
-DIRECTIONS = DIRECTION.at_least(1)
-
-NODE_NAME = parsy.letter.times(3).concat()
-
-NODE_LINE = parsy.seq(
-    NODE_NAME
-    .skip(parsers.SPACES)
-    .skip(parsy.string('='))
-    .skip(parsers.SPACES)
-    .skip(parsy.string('(')),
-    NODE_NAME
-    .skip(parsy.string(','))
-    .skip(parsers.SPACES),
-    NODE_NAME
-    .skip(parsy.string(')')),
-)
-
-CONTENT = parsy.seq(
-    DIRECTIONS
-    .skip(parsers.NEWLINES),
-    NODE_LINE
+CONTENT = (
+    parsers.NUMBER_LIST
     .sep_by(parsers.NEWLINE)
     .skip(parsers.NEWLINE.many())
 )
 
 
-@dataclass(frozen=True, kw_only=True)
-class Node:
-    left: str
-    right: str
+def has_all_zeros(*, history: Sequence[int]) -> bool:
+    return all(value == 0 for value in history)
 
 
-@dataclass(frozen=True, kw_only=True)
-class Network:
-    directions: str
-    nodes_by_name: Dict[str, Node]
-
-
-def parse_network(*, content: str) -> Network:
-    directions, nodes = CONTENT.parse(content)
-
-    nodes_by_name = {}
-    for (node_name, left, right) in nodes:
-        nodes_by_name[node_name] = Node(
-            left=left,
-            right=right,
-        )
-
-    return Network(
-        directions=directions,
-        nodes_by_name=nodes_by_name,
+def calculate_diff(*, history: Sequence[int]) -> Sequence[int]:
+    return tuple(
+        history[i + 1] - history[i]
+        for i in range(len(history) - 1)
     )
 
 
-def follow_direction(*, current_node_name: str, direction: str, nodes_by_name: Dict[str, Node]) -> str:
-    current_node = nodes_by_name[current_node_name]
+def calculate_next_value(*, history: Sequence[int]) -> int:
+    if has_all_zeros(history=history):
+        return 0
 
-    match direction:
-        case 'L':
-            return current_node.left
-        case 'R':
-            return current_node.right
-        case _:
-            raise Exception(f'unexpected direction: {direction}')
+    diff = calculate_diff(history=history)
+    next_diff_value = calculate_next_value(history=diff)
 
-
-def follow_directions(*, current_node_name: str, directions: Sequence[str], nodes_by_name: Dict[str, Node]) -> str:
-    current = current_node_name
-    for direction in directions:
-        current = follow_direction(
-            current_node_name=current,
-            direction=direction,
-            nodes_by_name=nodes_by_name,
-        )
-
-    return current
-
-
-def count_steps(*, network: Network) -> int:
-    current = 'AAA'  # start node name
-    step_count = 0
-    while current != 'ZZZ':  # end node name
-        current = follow_directions(
-            current_node_name=current,
-            directions=network.directions,
-            nodes_by_name=network.nodes_by_name,
-        )
-        step_count += len(network.directions)
-
-    return step_count
+    return next_diff_value + history[-1]
 
 
 def solution(content: str, /) -> int:
-    network = parse_network(content=content)
-    return count_steps(network=network)
+    histories = CONTENT.parse(content)
+
+    return sum(
+        calculate_next_value(history=history)
+        for history in histories
+    )
 
 
 def main():
