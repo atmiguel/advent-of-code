1a4d34a88f37e35379437c96eff973af51860771
Adrian Miguel <adriantmiguel@gmail.com>
2023-12-10 21:15:11 -0800

    saving

--- adventofcode/y2023/d10/p1/__init__.py
+++ adventofcode/y2023/d10/p1/__init__.py
@@ -1,45 +1,84 @@
-from typing import Sequence
+from dataclasses import dataclass
+import parsy
+from typing import Sequence, Tuple, Optional
 
 from adventofcode.helpers import executor, parsers
 
 
+PIPE = parsy.regex(r'[\|\-LJ7F\.S]')
+
 CONTENT = (
-    parsers.NUMBER_LIST
+    PIPE.at_least(1).concat()
     .sep_by(parsers.NEWLINE)
     .skip(parsers.NEWLINE.many())
 )
 
 
-def has_all_zeros(*, history: Sequence[int]) -> bool:
-    return all(value == 0 for value in history)
+@dataclass(frozen=True, kw_only=True)
+class Pipe:
+    directions: Tuple[str, str]
 
 
-def calculate_diff(*, history: Sequence[int]) -> Sequence[int]:
-    return tuple(
-        history[i + 1] - history[i]
-        for i in range(len(history) - 1)
-    )
+@dataclass(frozen=True, kw_only=True)
+class Grid:
+    pipes: Sequence[Sequence[Optional[Pipe]]]
+    start: Tuple[int, int]
 
 
-def calculate_next_value(*, history: Sequence[int]) -> int:
-    if has_all_zeros(history=history):
-        return 0
+def parse_grid(*, content: str) -> Grid:
+    grid = CONTENT.parse(content)
 
-    diff = calculate_diff(history=history)
-    next_diff_value = calculate_next_value(history=diff)
+    pipes = []
+    start = None
+    for row_index, line in enumerate(grid):
+        pipe_row = []
 
-    return next_diff_value + history[-1]
+        for column_index, pipe in enumerate(line):
+            value: Optional[Pipe]
+            match pipe:
+                case '|':
+                    value = Pipe(directions=('up', 'down'))
+                case '-':
+                    value = Pipe(directions=('left', 'right'))
+                case 'L':
+                    value = Pipe(directions=('up', 'right'))
+                case 'J':
+                    value = Pipe(directions=('up', 'left'))
+                case '7':
+                    value = Pipe(directions=('down', 'left'))
+                case 'F':
+                    value = Pipe(directions=('down', 'right'))
+                case '.':
+                    value = None
+                case 'S':
+                    value = None
+                    if start is not None:
+                        raise Exception('expected a single start character')
+
+                    start = (row_index, column_index)
+                case _:
+                    raise Exception('unexpected character')
+
+            pipe_row.append(value)
+
+        pipes.append(pipe_row)
+
+    if start is None:
+        raise Exception('expected a single start character')
+
+    return Grid(pipes=pipes, start=start)
 
 
 def solution(content: str, /) -> int:
-    histories = CONTENT.parse(content)
+    grid = parse_grid(content=content)
+    print(grid)
 
-    return sum(
-        calculate_next_value(history=history)
-        for history in histories
-    )
+    # return sum(
+    #     calculate_next_value(history=history)
+    #     for history in histories
+    # )
 
 
 def main():
     executor.execute_example(solution)
-    executor.execute_actual(solution)
+    # executor.execute_actual(solution)
