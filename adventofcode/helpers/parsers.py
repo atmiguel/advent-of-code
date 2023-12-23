4b2c67632cecf20469ad7723c6ddde146e442998
Adrian Miguel <adriantmiguel@gmail.com>
2023-12-22 18:17:42 -0800

    saving

--- adventofcode/helpers/parsers.py
+++ adventofcode/helpers/parsers.py
@@ -7,5 +7,7 @@ NEWLINES = NEWLINE.at_least(1)
 SPACE = parsy.string(' ')
 SPACES = SPACE.at_least(1)
 
+DIGIT = parsy.decimal_digit.map(int)
+
 NUMBER = parsy.regex(r'-?\d+').map(int)
 NUMBER_LIST = NUMBER.sep_by(SPACES)
